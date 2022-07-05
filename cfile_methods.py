913dfc976173af745bd337fe61537bf85f17c4c7
John Su <johnchangsu@gmail.com>
2022-07-05 10:10:28 +1000

    Changed commands to an obj so cfile_methods is just a script containing functions
    Added:
    Rotate
    Viewpoint

--- cfile_methods.py
+++ cfile_methods.py
@@ -13,30 +13,51 @@ Only Use this with the cfile object!!
 This contains all the methods to write to the cfile file.
 
 '''
-class cfileMethods():
-    def __init__(self):
-        pass
-    def setOpenFile(self,dsplotName):
-    
-        return f'open d3plot "{self.cwd}{dsplotName}"'
-    
-    def setInfo(self,author):
-        if author:
-            self.author = author
-        
-        return f'#$ LS-PrePost command file created by {author if author else "University of Sydney"}\n#$ Created on {self.timeCreated}'
 
 
-    def screenshot(self,imgName = None,window = 'OGL1x', gamma = 1.24 ,invert = 0.9,overwrite = False):
-    # print png "C:\Users\ShyGuy\Desktop\M06 - Post-Processing\LS-PrePost_2020R1_EN_WI06\image_004.png" gamma 1.40 invert 0.90 enlisted "OGL1x
-        filename ,file_ext = imgName.split('.')
-        #Dont overwrite and check if file already exists
-        if not overwrite and imgName in os.listdir(self.cwd):
-            imgName = filename+'_'+len(glob(f'{filename}*.{file_ext}'))+'.' + file_ext
-        return f'print {file_ext} "{self.cwd}{imgName}" gamma {gamma} invert {invert} enlisted "{window}"'
+def setOpenFile(dsplotName,cwd):
+
+    return f'open d3plot "{cwd}{dsplotName}"'
+
+def setInfo(author = None):
+    return f'#$ LS-PrePost command file created by {author if author else "University of Sydney"}\n#$ Created on {datetime.today().strftime("%Y-%m-%d %H:%M:%S")}'
 
 
-    def setContour(self,val):
-        pass
-    def setCountorOptions(self):
-        pass
+def screenshot(imgName,cwd = os.getcwd(),window = 'OGL1x', gamma = 1.24 ,invert = 0.9,overwrite = False):
+# print png "C:\Users\ShyGuy\Desktop\M06 - Post-Processing\LS-PrePost_2020R1_EN_WI06\image_004.png" gamma 1.40 invert 0.90 enlisted "OGL1x
+    filename ,file_ext = imgName.split('.')
+    valid_formats = {'png','jpeg','jpg','bmp','tiff'}
+    if file_ext not in valid_formats:
+        ValueError(f'Please use a supported file firmat: \n{valid_formats}')
+
+    #Dont overwrite and check if file already exists
+    if not overwrite and imgName in os.listdir(cwd):
+        imgName = filename+str(len(glob(f'{filename}\b\d{1,2}\b.{file_ext}'))+1)+'.' + file_ext
+    return f'print {file_ext} "{cwd}{imgName}" gamma {gamma} invert {invert} enlisted "{window}"'
+
+
+def viewpoint(view = 'top'):
+    valid_views = {'top','bottom','front','back','left','right'} #Need to check
+    if view in valid_views:
+        return view
+    else:
+        raise ValueError(f'view give is invalid. Check speslling or please choose a view from the following options: \n{valid_views}')
+
+def zoom(x,y,z):
+    pass
+
+def rotate(angle,axis = 'X'):
+    axis = axis.lower()
+    valid_axis = {'x','y','z'}
+
+    if not (isinstance(angle,float) or isinstance(angle,int)):
+        raise ValueError('Enter a number (int or float) for angle')
+
+    if axis not in valid_axis:
+        raise ValueError('valid Axis\' are x,y or z')
+
+    return f'r{axis} {angle}'
+def setContour(self,val):
+    pass
+def setCountorOptions(self):
+    pass
